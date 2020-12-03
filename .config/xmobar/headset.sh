66796de3a8ff1bee7a00948e0f42937161316337
Samuel Walker <ldspcjunkie@gmail.com>
2020-12-03 16:28:01 -0700

    small updates

--- .config/xmobar/headset.sh
+++ .config/xmobar/headset.sh
@@ -8,12 +8,21 @@ else
     var=$(headsetcontrol -b -c)
     if [[ "$var" -eq "-1" ]]
     then
-	echo "Charging"
+	echo "<fc=darkblue>Charging</fc>"
     else if [[ "$var" -eq "-2" ]]
         then
-	    echo "Not Connected"
+	    echo ""
         else
-	    echo "$var%"
+	    if [[ "$var" -ge "50" ]]
+	    then
+		echo "<fc=darkgreen>$var</fc>%"
+	    else if [[ "$var" -le "20" ]]
+		 then
+		     echo "<fc=darkred>$var</fc>%"
+		 else
+	             echo "<fc=darkorange>$var</fc>%"
+		 fi
+	    fi
         fi
     fi
 fi
